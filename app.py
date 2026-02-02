48a26719905ed1822e456e0a59b0b9adacee12d6
thavasiGitup <thavasi1511@gmail.com>
2026-02-02 15:56:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+import pickle
+import os
+import numpy as np
 
 app = FastAPI()
 
+MODEL_PATH = "/home/site/wwwroot/loan_model.pkl"
+
+@app.on_event("startup")
+def load_model():
+    global model
+    with open(MODEL_PATH, "rb") as f:
+        model = pickle.load(f)
+
 @app.get("/")
 def home():
-    return {"status": "App is running on Azure"}
+    return {"message": "Loan Approval ML API is running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    features = np.array([list(data.values())])
+    prediction = model.predict(features)
+    return {"loan_approval": int(prediction[0])}
